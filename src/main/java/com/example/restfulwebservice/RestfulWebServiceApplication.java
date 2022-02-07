b0a5c5064c753baf6f0c76e09e4356f6012cad0d
dante <ejrlfs12@naver.com>
2022-02-07 23:40:47 +0900

    Initial commit

---
+++ src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
@@ -0,0 +1,31 @@
+package com.example.restfulwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulWebServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestfulWebServiceApplication.class, args);
+    }
+
+}
+
+
+# restful-web-service
+
+        ## 환경 설정
+
+        ###Spring Boot를 이용한 RESTFul Web Service
+
+        Java : 11
+
+
+        Spring Boot : 3.0.0-M1
+
+        dependency : spring web, lombok, Spring Boot DevTools, Spring Data JPA, H2 Database
+
+        type : Maven project
+
+        Packaging : Jar
