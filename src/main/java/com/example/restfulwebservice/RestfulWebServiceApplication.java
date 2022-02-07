1d3273a46f9450f5379f6125853daca95c187f3d
dante <ejrlfs12@naver.com>
2022-02-08 00:24:38 +0900

    HelloWorld Controller 추가

--- src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
+++ src/main/java/com/example/restfulwebservice/RestfulWebServiceApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class RestfulWebServiceApplication {
 
+
     public static void main(String[] args) {
         SpringApplication.run(RestfulWebServiceApplication.class, args);
     }
@@ -13,19 +14,3 @@ public class RestfulWebServiceApplication {
 }
 
 
-# restful-web-service
-
-        ## 환경 설정
-
-        ###Spring Boot를 이용한 RESTFul Web Service
-
-        Java : 11
-
-
-        Spring Boot : 3.0.0-M1
-
-        dependency : spring web, lombok, Spring Boot DevTools, Spring Data JPA, H2 Database
-
-        type : Maven project
-
-        Packaging : Jar
